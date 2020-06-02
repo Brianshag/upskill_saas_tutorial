1f44bc2bfd2fb41364df2e46f500f07df2cdf40d
Brianshag <brian.shagalovich@gmail.com>
2020-06-02 18:10:43 +0000

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
  get 'about', to: 'pages#about'
- resources :contacts
- get 'contact-us', to: 'contacts#new' #custom url code, lesson 115
+ resources :contacts, only: :create #we only need the create resource
+ get 'contact-us', to: 'contacts#new', as: 'new_contact' #custom url code, lesson 115, the contacts#new is path name found from doing command rails routes, lesson 118 adding the as:... to change path name, to keep same as in the application.html.erb file
 end
